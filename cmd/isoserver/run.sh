eceeb5f1c4eff457d56afb37641fc5b9cfd4f151
Junlapong L <junlapong@gmail.com>
2021-07-03 05:48:03 +0700

    Add isoserver/run.sh

---
+++ cmd/isoserver/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#---- Running ISO Server .. ----
+go run isoserver.go \
+   -specs-dir ../../test/testdata/specs \
+   --def-file ../../test/testdata/appdata/2/IsoMiniSpec_Server_01.srvdef.json
