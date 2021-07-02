443bf9b179c3cc8b70fbae9d04e264b7b2301ecc
Junlapong L <junlapong@gmail.com>
2021-07-02 11:37:46 +0700

    Add run.sh

---
+++ cmd/isosim/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# ---- Starting ISO Websim .. ----
+# ---- Setting ENV variables -----
+
+TLS_ENABLED=false
+TLS_CERT_FILE=/path/to/isosim/certs/cert.pem
+TLS_KEY_FILE=/path/to/isosim/certs/key.pem
+
+# ---- Starting App -----
+
+go run . -http-port 8080 --log-level TRACE -specs-dir ../../test/testdata/specs -html-dir ../../web -data-dir ../../test/testdata/appdata
+
